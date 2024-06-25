cfb5d4b25003393485e9f18f01eab6b801ea0773
Mattia Braga <mattiabraga2006@gmail.com>
2024-06-25 10:30:13 +0200

    ToFrame done with implementation in the app

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 # fluffy-value-converter
 
+![Language](https://img.shields.io/badge/Spellcheck-Pass-green?style=flat)
+![Language](https://img.shields.io/badge/Language-Python-yellowgreen?style=flat)
+![Testing](https://img.shields.io/badge/PEP8%20Check-Passing-green)
+![Testing](https://img.shields.io/badge/Test-Pass-green)
+
 ## Description
 
 ## Requirements
 
+python >= 3.8
+
+**Setting Up the Environment**
+
+* Windows: `./start_Windows.bat`
+* Linux/macOS: `./start_Linux.sh`
+
+These scripts will install required dependencies, and build a virtual environment for you if you don't have one. They will also start the website automatically.
+
+## Running the Program (CLI or GUI)
+
+### CLI
+
+1. Navigate to the `bin` directory: `cd bin`
+
+2. Execute `python webpage.py [--help]` (use `python3` on Linux/macOS) in your terminal
+
+    The `--help` flag displays available command-line arguments.
+
+### GUI
+
+Double click on the *start files* for your OS, and it should open the 'web app' automatically after setting up the environment, like I wrote in the **Requirements** area.
+
 ## Author
+
+Neetre
--- bin/main.py
+++ bin/main.py
@@ -46,16 +46,120 @@ class Currency_manager:
         num = num * to_.value
         valore = f"{num:.2f}"
         return valore
-    
 
+
+class FromFrame(customtkinter.CTkFrame):
+    def __init__(self, master, title, cur):
+        super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
+        self.title = title
+        self.choice = ""
+        self.text = ""
+
+        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
+
+        self.combobox = customtkinter.CTkComboBox(self, values=cur, command=self.combobox_callback)
+        self.combobox.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+
+        self.textbox = customtkinter.CTkTextbox(master=self, height=1, width=1, border_width=3, corner_radius=6)
+        self.textbox.grid(row=3, column=0, padx=10, pady=(10, 0), sticky="nsew")
+        self.textbox.insert("0.0", "")
+
+    def combobox_callback(self, choice):
+        print("Combobox dropdown clicked: ", choice)
+
+    def get_choice(self):
+        choice = self.combobox.get()
+        text = self.textbox.get("0.0", "end")
+        return choice, text
+
+
+class ToFrame(customtkinter.CTkFrame):
+    def __init__(self, master, title, cur, value):
+        super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
+        self.title = title
+        self.choice = ""
+        self.text = ""
+
+        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
+
+        self.combobox = customtkinter.CTkComboBox(self, values=cur, command=self.combobox_callback)
+        self.combobox.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+
+        self.textbox = customtkinter.CTkTextbox(master=self, height=1, width=1, border_width=3, corner_radius=6)
+        self.textbox.grid(row=3, column=0, padx=10, pady=(10, 0), sticky="nsew")
+        self.textbox.insert("0.0", f"{value}")
+
+    def combobox_callback(self, choice):
+        print("combobox dropdown clicked: ", choice)
+
+    def get_choice(self):
+        choice = self.combobox.get()
+        return choice
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.result = ''
+        self.curr_manager = Currency_manager()
+        f_csv = "../data/currencies.csv"
+        self.curr_manager.estraz(f_csv)
+        
+        self.title("Convertor")
+        self.geometry("640x480")
+        self.grid_columnconfigure((0, 2), weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.from_frame = FromFrame(self, "From", self.curr_manager.values)
+        self.from_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
+
+        self.label = customtkinter.CTkLabel(self, text="=")
+        self.label.grid(row=0, column=2, padx=10, pady=(10, 0), sticky="nsew")
+
+        self.to_frame = ToFrame(self, "To", self.curr_manager.values, self.result)
+        self.to_frame.grid(row=3, column=0, padx=10, pady=10, sticky="ew", columnspan=3)
+
+        self.button = customtkinter.CTkButton(self, text="Convert", command=self.button_callback)
+        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew", columnspan=3)
+
+    def button_callback(self):
+        print(f"1: {self.from_frame.get_choice()[0]} ; {self.from_frame.get_choice()[1]}")
+        print(f"2: {self.to_frame.get_choice()}")
+        self.elabor()
+        self.to_frame = ToFrame(self, "To", self.curr_manager.values, self.result)
+        self.to_frame.grid(row=0, column=2, padx=10, pady=(10, 0), sticky="nsew")
+    
+    def app_get(self):
+        name_values = (self.from_frame.get_choice()[0], self.to_frame.get_choice())
+        value_to_convert = self.from_frame.get_choice()[1]
+        return name_values, value_to_convert
+
+    def elabor(self):
+        name_values, value_to_convert = self.app_get()
+        value_to_convert = int(value_to_convert.replace("\n", ""))
+        self.result = self.curr_manager.elab(name_values[0], name_values[1], value_to_convert)
+
+'''
 def main():
     f_csv = "../data/currencies.csv"
-
+    from_="US Dollar"
+    to_ = "Brazilian Real"
+    value = float(5)
+    print(f"Conversion from {from_} to {to_}, value {value}. Result:")
     curr_manager = Currency_manager()
     curr_manager.estraz(f_csv)
-    result = curr_manager.conversion("US Dollar", "Brazilian Real", float(5))
+    result = curr_manager.conversion(from_, to_, value)
     print("Conversion: ", result)
+'''
 
+def main():
+    app = App()
+    app.mainloop()
+    return
 
 if __name__ == "__main__":
     main()
